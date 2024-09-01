7f08a43c7b992786d505adcf88d3e07c7032eafb
Idan <idan.nechemia@gmail.com>
2024-09-01 15:19:04 +0300

    conncet to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+mongoose.connect(process.env.Mongo).then(() =>{
+    console.log('Connected');
+}).catch((err) =>{
+    console.log(err)
+})
 const app = express();
 
 
